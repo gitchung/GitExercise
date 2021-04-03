8f36ac7a8b35485f33a33e6d959449c7718fb59a
gitchung <guriun@gmail.com>
2021-04-03 17:32:56 +0900

    Initial Commit

---
+++ GitExercise/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitExercise
+//
+//  Created by JI Yong Jung on 2021/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
